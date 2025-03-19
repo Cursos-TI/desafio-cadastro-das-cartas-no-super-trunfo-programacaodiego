8ca024184b1cdcdeadcba3b5204ccde961fe5c2d
programacaodiego <diego.felix525@gmail.com>
2025-03-19 15:39:55 +0000

    cadastro de carta SuperTrunfo

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -4,4 +4,12 @@ int main () {
 printf ("desafio xadrez \n");
 printf ("novo commit \n");
 
+printf ("estado A \n"); 
+printf ("código da carta [A 01] \n");
+printf ("nome da cidade São Paulo \n");
+printf ("população 12325000 \n");
+printf ("Área 1521.11 km² \n");
+printf ("PIB 699.28 bilhões de reais \n");
+printf ("Número de Pontos Turísticos 50 \n");
+
 }
